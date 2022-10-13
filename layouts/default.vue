2cd9e12161878c4411e8ed652aedecc0f484991f
sakaguchi-diverta <sakaguchi@diverta.co.jp>
2022-10-13 23:21:22 +0900

    v-html

---
+++ layouts/default.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <header><h1>Static site</h1></header>
+    <nuxt />
+    <footer>sakaguchi-diverta</footer>
+  </div>
+</template>
---
+++ pages/static_page/_slug.vue
@@ -0,0 +1,17 @@
+<template>
+  <div v-html="html">
+</template>
+
+<script>
+export default {
+  async asyncData({ $axios }) {
+    const html = await $axios.get('/rcms-api/5/static_page/static1').then((res) => {
+      return res.data.details.html;
+    })
+    console.log(html);
+    return {
+      html
+    }
+  },
+}
+</script>
